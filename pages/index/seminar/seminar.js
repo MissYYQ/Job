977e15c2731f3814f1ed570d5a5b3e732034e7e9
Miss_Y <2726256822@qq.com>
2020-11-10 21:07:16 +0800

    seminar

--- pages/index/seminar/seminar.js
+++ pages/index/seminar/seminar.js
@@ -4,17 +4,39 @@ Page({
    * 页面的初始数据
    */
   data: {
-    item:0,
+    seminarList:[
+      {
+        logoImgUrl:"/images/index/hotCompany.png",
+        companyName:"云程科技",
+        school: "九江学院",
+        position: "望庐楼407",
+        date: "2020-11-11",
+        time: "10:30",
+      },
+      {
+        logoImgUrl:"/images/index/hotCompany.png",
+        companyName:"云程科技",
+        school: "九江学院",
+        position: "望庐楼407",
+        date: "2020-11-11",
+        time: "10:30",
+      },
+      {
+        logoImgUrl:"/images/index/hotCompany.png",
+        companyName:"云程科技",
+        school: "九江学院",
+        position: "望庐楼407",
+        date: "2020-11-11",
+        time: "10:30",
+      },
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var kind = options.kind;
-    this.setData({
-      item: kind
-    })
+
   },
 
     //搜索内容
@@ -29,6 +51,13 @@ Page({
       this.setData({
         inputVal: null
       });
+    },
+
+    //跳转至宣讲会详情页面
+    toSeminarDetailsPage:function(){
+      wx.navigateTo({
+        url: '/pages/index/seminar/seminarDetails/seminarDetails',
+      })
     },
 
 
---
+++ pages/index/seminar/seminarDetails/seminarDetails.js
@@ -0,0 +1,74 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    email: "hr@163.com",
+    seminar : {
+      logoImgUrl: "/images/index/hotCompany.png",
+      companyName: "云程科技",
+      industry: "计算机软件",
+      school: "九江学院",
+      position: "望庐楼407",
+      date: "2020-11-11",
+      time: "10:30",
+    },
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
