7e6e890669e7a6f3fa4448ac7f5218ecb64f187a
Miss_Y <2726256822@qq.com>
2020-11-21 15:15:11 +0800

    职位管理

--- pages/mine/companyMine/companyJob/companyJob.js
+++ pages/mine/companyMine/companyJob/companyJob.js
@@ -1,11 +1,56 @@
-// pages/mine/companyMine/companyJob/companyJob.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    jobData: [
+      {
+        name: "前端开发",
+        salary: "6-10K",
+        degree: "本科",
+        experience: "1-3年",
+        companyName: "云程科技",
+        industry: "计算机软件",
+        companySize: "20-99人",
+        financingStage: "B轮",
+        logoImgUrl: "/images/index/hotCompany.png",
+        kind: 0
+      },
+      {
+        name: "前端开发",
+        salary: "6-10K",
+        degree: "本科",
+        experience: "1-3年",
+        companyName: "云程科技",
+        industry: "计算机软件",
+        companySize: "20-99人",
+        financingStage: "B轮",
+        logoImgUrl: "/images/index/hotCompany.png",
+        kind: 0
+      },
+    ],
+  },
 
+  //搜索内容
+  inputVal: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
+
+  //清空搜索
+  clearInput: function () {
+    this.setData({
+      inputVal: null
+    });
+  },
+
+  //跳转至职位详情页面
+  toJobDetailsPage: function () {
+    wx.navigateTo({
+      url: '/pages/index/jobDetails/jobDetails',
+    })
   },
 
   /**
@@ -15,52 +60,4 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
