4284de1d6d5e6d3f010432cc3712514b20ce189b
Miss_Y <2726256822@qq.com>
2020-11-22 10:52:29 +0800

    投递管理

--- pages/mine/companyMine/deliveryDeal/deliveryDeal.js
+++ pages/mine/companyMine/deliveryDeal/deliveryDeal.js
@@ -1,66 +1,57 @@
-// pages/mine/companyMine/deliveryDeal/deliveryDeal.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    //1-投递、2-通过、3-面试、4-接受、5-拒绝、6-未通过
+    delivery: [{
+        jobName: "前端开发工程师",
+        studentName: "南笙",
+        resumeFile: {},
+        status: 1,
+      },
+      {
+        jobName: "前端开发工程师",
+        studentName: "南笙",
+        resumeFile: {},
+        status: 1,
+      },
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      resumeFile: wx.getStorageSync('resumeFile')
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //查看职位
+  toJobDetailsPage: function () {
+    wx.navigateTo({
+      url: '/pages/index/jobDetails/jobDetails',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  //查看在线简历
+  toPreviewResumePage: function () {
+    wx.navigateTo({
+      url: '/pages/mine/studentMine/resume/previewResume/previewResume',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //查看附件简历
+  openResumeFile: function () {
+    var that = this;
+    wx.openDocument({
+      filePath: that.data.resumeFile.path,
+      success: function (res) {
+        console.log('成功打开简历附件')
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
