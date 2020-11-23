4d6c0aff0ea844f6d82273d0b313e80d4f9ec523
Miss_Y <2726256822@qq.com>
2020-11-23 11:01:12 +0800

    邀请面试

--- pages/mine/companyMine/deliveryDeal/deliveryDeal.js
+++ pages/mine/companyMine/deliveryDeal/deliveryDeal.js
@@ -1,9 +1,12 @@
+var util = require('../../../../utils/util');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    showTemplate: false,
     //1-投递、2-通过、3-面试、4-接受、5-拒绝、6-未通过
     delivery: [{
         jobName: "前端开发工程师",
@@ -15,7 +18,7 @@ Page({
         jobName: "前端开发工程师",
         studentName: "南笙",
         resumeFile: {},
-        status: 1,
+        status: 2,
       },
     ]
   },
@@ -26,6 +29,11 @@ Page({
   onLoad: function (options) {
     this.setData({
       resumeFile: wx.getStorageSync('resumeFile')
+    })
+    //获取当前日期
+    var nowDate = util.formatDate(new Date());
+    this.setData({
+      nowDate: nowDate,
     })
   },
 
@@ -53,5 +61,56 @@ Page({
       }
     })
   },
+
+  //邀请面试模板弹出
+  showTemplate: function () {
+    this.setData({
+      showTemplate: true
+    })
+  },
+
+  //日期picker改变
+  dateChange: function (e) {
+    let date = "addInterview.date"
+    this.setData({
+      [date]: e.detail.value,
+    })
+    wx.setStorageSync('addInterview', this.data.addInterview)
+  },
+
+  //时间picker改变
+  timeChange: function (e) {
+    let time = "addInterview.time"
+    this.setData({
+      [time]: e.detail.value
+    })
+    wx.setStorageSync('addInterview', this.data.addInterview)
+  },
+
+  //添加面试信息
+  addInterview: function (e) {
+    var key = e.currentTarget.dataset.key;
+    let name = "addInterview." + key;
+    this.setData({
+      [name]: e.detail.value
+    });
+    wx.setStorageSync('addInterview', this.data.addInterview);
+  },
+
+  //取消
+  cancelBtn:function(){
+    this.setData({
+      addInterview: null,
+      showTemplate: false
+    })
+    wx.setStorageSync('addInterview', this.data.addInterview)
+  },
+
+  //确定
+  determineBtn: function(){
+    this.setData({
+      showTemplate: false
+    })
+  }
 
 })
