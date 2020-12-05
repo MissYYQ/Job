91eefad5f687e6409712c491a6ba18954ddd93e5
Miss_Y <2726256822@qq.com>
2020-12-05 21:13:58 +0800

    求职攻略&&热门搜索

--- pages/index/hotCompany/hotCompany.js
+++ pages/index/hotCompany/hotCompany.js
@@ -51,6 +51,47 @@ Page({
     });
   },
 
+  //搜索
+  search: function () {
+    var key = this.data.inputVal
+    if (key != "") {
+      var that = this;
+      wx.request({
+        url: 'http://localhost:81/company/hotCompanySearch',
+        method: 'get',
+        data: {
+          key: key
+        },
+        success: function (res) {
+          console.log("搜索成功");
+          console.log(res.data);
+          that.setData({
+            company: res.data,
+          })
+          //字符串转数组
+          let length = res.data.length;
+          for (var i = 0; i < length; i++) {
+            if (that.data.company[i].welfare) {
+              var welfareArr = that.data.company[i].welfare.split("、");
+              let welfare = "company[" + i + "].welfare"
+              that.setData({
+                [welfare]: welfareArr,
+              })
+            }
+          }
+        },
+      })
+    } else {
+      wx.showToast({
+        title: '搜索内容不能为空！',
+        icon: 'none',
+        duration: 1500,
+        mask: true
+      })
+    }
+
+  },
+
   //跳转至公司详情页面
   toCompanyDetailsPage: function (e) {
     var id = e.currentTarget.dataset.id;
--- pages/index/hotJob/hotJob.js
+++ pages/index/hotJob/hotJob.js
@@ -12,13 +12,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //后台获取数据
     var that = this;
     wx.request({
       url: 'http://localhost:81/job/hotJob',
       method: 'get',
       success: function (res) {
-        console.log("获取热门公司成功");
+        console.log("获取热门职位成功");
         console.log(res.data);
         that.setData({
           job: res.data,
@@ -39,6 +38,36 @@ Page({
     this.setData({
       inputVal: null
     });
+  },
+
+  //搜索
+  search: function () {
+    var key = this.data.inputVal
+    if (key != "") {
+      var that = this;
+      wx.request({
+        url: 'http://localhost:81/job/hotJobSearch',
+        method: 'get',
+        data: {
+          key: key
+        },
+        success: function (res) {
+          console.log("搜索成功");
+          console.log(res.data);
+          that.setData({
+            job: res.data,
+          })
+        },
+      })
+    } else {
+      wx.showToast({
+        title: '搜索内容不能为空！',
+        icon: 'none',
+        duration: 1500,
+        mask: true
+      })
+    }
+
   },
 
   //跳转至职位详情页面
