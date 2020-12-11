f9258d9b4fa2cf798b1a135fddb5ad741e3a21ba
Miss_Y <2726256822@qq.com>
2020-12-11 21:12:39 +0800

    待处理简历

--- pages/mine/companyMine/deliveryDeal/deliveryDeal.js
+++ pages/mine/companyMine/deliveryDeal/deliveryDeal.js
@@ -9,27 +9,25 @@ Page({
     showTemplate: false,
     addInterview: {},
     //1-投递、2-通过、3-面试、4-接受、5-拒绝、6-未通过
-    delivery: [{
-        jobName: "前端开发工程师",
-        studentName: "南笙",
-        resumeFile: {},
-        status: 1,
-      },
-      {
-        jobName: "前端开发工程师",
-        studentName: "南笙",
-        resumeFile: {},
-        status: 2,
-      },
-    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      resumeFile: wx.getStorageSync('resumeFile')
+    var that = this;
+    var companyId = wx.getStorageSync('companyId');
+    wx.request({
+      url: 'http://localhost:81/delivery/deal',
+      method: 'GET',
+      data: {
+        companyId: companyId
+      },
+      success: function (res) {
+        that.setData({
+          delivery: res.data,
+        })
+      }
     })
     //获取当前日期
     var nowDate = util.formatDate(new Date());
@@ -39,62 +37,95 @@ Page({
   },
 
   //查看职位
-  toJobDetailsPage: function () {
+  toJobDetailsPage: function (e) {
+    var id = e.currentTarget.dataset.id;
     wx.navigateTo({
-      url: '/pages/index/jobDetails/jobDetails',
-    })
-  },
-
-  //查看在线简历
-  toPreviewResumePage: function () {
-    wx.navigateTo({
-      url: '/pages/mine/studentMine/resume/previewResume/previewResume',
+      url: '/pages/index/jobDetails/jobDetails?id=' + id,
     })
   },
 
   //查看附件简历
-  openResumeFile: function () {
-    var that = this;
+  openResumeFile: function (e) {
+    var path = e.currentTarget.dataset.path;
     wx.openDocument({
-      filePath: that.data.resumeFile.path,
+      filePath: path,
       success: function (res) {
         console.log('成功打开简历附件')
       }
     })
   },
 
+  //通过
+  pass: function (e) {
+    var id = e.currentTarget.dataset.id;
+    wx.request({
+      url: 'http://localhost:81/delivery/pass',
+      method: 'GET',
+      data: {
+        id: id
+      },
+      success: function (res) {
+        if (res.data) {
+          //刷新当前页
+          var pages = getCurrentPages();
+          var beforePage = pages[pages.length - 1];
+          beforePage.onLoad();
+        }
+      }
+    })
+  },
+
+  //不通过
+  fail: function (e) {
+    var id = e.currentTarget.dataset.id;
+    wx.request({
+      url: 'http://localhost:81/delivery/fail',
+      method: 'GET',
+      data: {
+        id: id
+      },
+      success: function (res) {
+        if (res.data) {
+          //刷新当前页
+          var pages = getCurrentPages();
+          var beforePage = pages[pages.length - 1];
+          beforePage.onLoad();
+        }
+      }
+    })
+  },
+
   //邀请面试模板弹出
-  showTemplate: function () {
+  showTemplate: function (e) {
     this.setData({
-      showTemplate: true
+      showTemplate: true,
+      id: e.currentTarget.dataset.id,
+      userId: e.currentTarget.dataset.userid,
+      jobId: e.currentTarget.dataset.jobid,
     })
   },
 
   //日期picker改变
   dateChange: function (e) {
-    let date = "addInterview.date"
     this.setData({
-      [date]: e.detail.value,
+      ['addInterview.date']: e.detail.value,
     })
-    wx.setStorageSync('addInterview', this.data.addInterview)
   },
 
   //时间picker改变
   timeChange: function (e) {
-    let time = "addInterview.time"
     this.setData({
-      [time]: e.detail.value
+      ['addInterview.time']: e.detail.value
     })
-    wx.setStorageSync('addInterview', this.data.addInterview)
   },
 
   //地区picker改变
   regionChange: function (e) {
-    let region = "addInterview.region"
+    var value = e.detail.value;
+    let region = value[0]+value[1]+value[2];
     this.setData({
-      [region]: e.detail.value
+      ['addInterview.region']: region
     })
-    wx.setStorageSync('addInterview', this.data.addInterview)
   },
 
   //添加面试信息
@@ -104,20 +135,18 @@ Page({
     this.setData({
       [name]: e.detail.value
     });
-    wx.setStorageSync('addInterview', this.data.addInterview);
   },
 
   //取消
   cancelBtn: function () {
     this.setData({
-      addInterview: {},
       showTemplate: false
     })
-    wx.setStorageSync('addInterview', this.data.addInterview)
   },
 
   //确定
-  determineBtn: function () {
+  determineBtn: function (e) {
+    var that = this;
     //判空
     let addInterview = this.data.addInterview;
     if (addInterview.date == null) {
@@ -134,7 +163,14 @@ Page({
         duration: 1500,
         mask: true
       })
-    } else if (addInterview.location == null) {
+    } else if (addInterview.region == null) {
+      wx.showToast({
+        title: '面试地区不能为空！',
+        icon: 'none',
+        duration: 1500,
+        mask: true
+      })
+    } else if (addInterview.detailedaddress == null) {
       wx.showToast({
         title: '面试地点不能为空！',
         icon: 'none',
@@ -142,8 +178,41 @@ Page({
         mask: true
       })
     } else {
-      this.setData({
-        showTemplate: false
+      var userId = that.data.userId;
+      var jobId = that.data.jobId;
+      wx.request({
+        url: 'http://localhost:81/interview/add',
+        method: 'GET',
+        data: {
+          userId: userId,
+          jobId: jobId,
+          addInterview: addInterview,
+        },
+        success: function (res) {
+          if (res.data) {
+            //修改投递状态为面试
+            var id = that.data.id;
+            wx.request({
+              url: 'http://localhost:81/delivery/interview',
+              method: 'GET',
+              data: {
+                id: id
+              },
+              success: function (res) {
+                if (res.data) {
+                  //关闭面试模板弹窗
+                  that.setData({
+                    showTemplate: false
+                  })
+                  //刷新当前页
+                  var pages = getCurrentPages();
+                  var beforePage = pages[pages.length - 1];
+                  beforePage.onLoad();
+                }
+              }
+            })
+          }
+        }
       })
     }
   }
