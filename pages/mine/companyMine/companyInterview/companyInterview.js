fa8b51f38989f39bedd181533aba5b0a664f96e7
Miss_Y <2726256822@qq.com>
2020-12-14 14:38:34 +0800

    公司的面试

--- pages/mine/companyMine/companyInterview/companyInterview.js
+++ pages/mine/companyMine/companyInterview/companyInterview.js
@@ -14,15 +14,45 @@ Page({
    */
   onLoad: function (options) {
     //获取当前日期
-    var nowDateTime = util.formatDateTime(new Date());
+    var nowDate = util.formatDate(new Date());
+    var nowTime = util.formatTime(new Date());
     this.setData({
-      nowDateTime: nowDateTime,
+      nowDate: nowDate,
+      nowTime: nowTime,
+    })
+    //后台获取数据
+    var that = this;
+    wx.request({
+      url: 'http://localhost:81/interview/allByCompanyId',
+      method: 'GET',
+      data: {
+        companyId: wx.getStorageSync('companyId')
+      },
+      success: function (res) {
+        console.log(res.data);
+        that.setData({
+          interview: res.data
+        })
+      },
     })
   },
 
+  //跳转至职位详情页面
   toJobDetailsPage: function (e) {
+    var id = e.currentTarget.dataset.jobid;
+    //预览量加一
+    wx.request({
+      url: 'http://localhost:81/job/addPageviews',
+      method: 'post',
+      data: {
+        id: id
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+    })
     wx.navigateTo({
-      url: '/pages/index/jobDetails/jobDetails',
+      url: '/pages/index/jobDetails/jobDetails?id=' + id
     })
   },
 
